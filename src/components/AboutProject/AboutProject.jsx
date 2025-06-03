98c39992a24c0bf2a96fad159d7b4b131a15b645
DisturbedCherry <jakub.t.wislicki@gmail.com>
2025-06-03 22:55:02 +0200

    Corrected AboutProject

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -1,19 +1,19 @@
-import signlyLogo from '../assets/logo_final.png'
-import '../css/AboutProject.css'
+import signlyLogo from '../../assets/logo_final.png'
+import styles from './AboutProject.module.css'
 
 function AboutProject() {
     return (
-        <div className='about-project'>
-            <div className='text-container'>
-                <div className='title'>
+        <div className={styles.mainContent}>
+            <div className={styles.aboutContainer}>
+                <div className={styles.titleWrapper}>
                     O projekcie
                 </div>
-                <div className='content'>
+                <div className={styles.textWrapper}>
                     Signly - system nauki języka migowego, opracowany przez studentów informatyki na wydziale Elektrotechniki, Elektroniki, Informatyki i Automatyki Politechniki Łodzkiej w ramach projektu kompetencyjnego
                 </div>
             </div>
-            <div className='img-container'>
-                <img className='logo' src={signlyLogo}/>
+            <div className={styles.logoWrapper}>
+                <img className={styles.logo} src={signlyLogo} />
             </div>
         </div>
     )
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,5 +1,5 @@
 import Header from '../components/Header';
-import AboutProject from '../components/AboutProject';
+import AboutProject from '../components/AboutProject/AboutProject';
 
 function WelcomePage() {
 
